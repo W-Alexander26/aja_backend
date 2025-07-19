c141b2f8463421ce747f5f0f8076e4bd92c4b319
Javier <jjaviervgg@gmail.com>
2025-07-18 21:56:35 -0600

    feat(repo):Primer commit

---
+++ aja/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+   imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env'
+      }),
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        autoLoadEntities: true,
+        synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
